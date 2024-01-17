3fbfe8b46ed642bbe88d4e30673341a7e567cad5
Ustinya Petrova <ust.petrova@gmail.com>
2024-01-17 17:58:16 +0100

    fix: renovate method getAll()

--- src/main/java/data_jpa_homework/task_1/repositories/BankTransactionRepository.java
+++ src/main/java/data_jpa_homework/task_1/repositories/BankTransactionRepository.java
@@ -2,7 +2,9 @@ package data_jpa_homework.task_1.repositories;
 
 import data_jpa_homework.task_1.domain.BankTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BankTransactionRepository extends JpaRepository<BankTransaction, Integer> {
 
 }
--- src/main/java/data_jpa_homework/task_1/services/BankTransactionService.java
+++ src/main/java/data_jpa_homework/task_1/services/BankTransactionService.java
@@ -4,6 +4,7 @@ import data_jpa_homework.task_1.domain.BankTransaction;
 import data_jpa_homework.task_1.repositories.BankTransactionRepository;
 import java.util.List;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Service
 public class BankTransactionService {
