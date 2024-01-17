685d48238e02c7621aff8437fb96c49513371c17
Ustinya Petrova <ust.petrova@gmail.com>
2024-01-17 16:32:36 +0100

    initial commit

---
+++ src/test/java/data_jpa_homework/task_1/Task1ApplicationTests.java
@@ -0,0 +1,13 @@
+package data_jpa_homework.task_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
